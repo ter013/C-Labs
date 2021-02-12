810f38b953ddcb55b3dc511e07d83beccfffcca0
ter013 <71257830+ter013@users.noreply.github.com>
2021-02-12 12:26:37 +0300

    Add files via upload

---
+++ Lab2problem4.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int prime(int n)
+{
+    int x = 1;
+    int k = 0;
+    int i = 0;
+    while (i < n){
+        x++;
+        for (int j = 2; j < x + 1; j++){
+            if (x % j == 0){
+                k++;
+            }
+        }
+        if (k == 1){
+            i++;
+        }
+        k = 0;
+    }
+    return x;
+}
+
+int main()
+{
+    int n, c;
+    cin >> n;
+    c = prime(n);
+    cout << c;
+}
